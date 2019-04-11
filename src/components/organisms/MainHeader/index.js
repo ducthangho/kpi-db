26cd778a3afdf8911b92bee0ee0ab204b012dedb
ducthangho <ducthangho@gmail.com>
2019-04-11 21:23:49 +0700

    Fix header

--- src/components/organisms/MainHeader/index.js
+++ src/components/organisms/MainHeader/index.js
@@ -6,7 +6,8 @@ import React, {
 // import styled from "styled-components";
 // import { size } from "styled-theme";
 import { getState, getStore } from "../../../state";
-import { Switch, Row, Col, Typography, Layout, Input } from "antd";
+import { Switch, Row, Col, Typography, Layout, Input,Divider } from "antd";
+import intl from 'react-intl-universal';
 import {
     observer,    
 } from 'mobx-react-lite';
@@ -41,30 +42,37 @@ const MainHeader = observer(props => {
   };
 
   return (
-    <Header style={{ background: "#E0E0E0", padding: 0 }}>
-      <Row>
+    <Header style={{ background: "#21224d", padding: 0, height: 50, margin: 0 }}>
+      <Row style={{ background: "#21224d", padding: 0, height: 50, margin: 0 }}>
         <Col span={6}>
           <Title
             level={3}
             style={{
-              margin: 14,
+              margin: 0,
               borderRight: "#ccc 2px solid",
-              paddingLeft: 10
+              paddingLeft: 30,
+              paddingTop: 5,
+              color: "white",
+              height: 50
             }}
           >
-            MY DASHBOARD
+            Tăng trưởng kinh tế
           </Title>
         </Col>
-        <Col span={18}>
-          <Search
-            placeholder="Ask a question"
-            onSearch={onSearch}
-            style={{ width: 250, paddingTop: 14 }}
-            enterButton
-          />
-          <span className="ant-divider" style={{ margin: "0 1em" }} />
-          <strong>SHOW COLOR</strong> &nbsp;
-          <Switch defaultChecked={dashboard.showColor} onChange={onShowColor} />
+        <Col>
+        <Title style={{
+          fontFamily: 'SegoeUI',
+          fontSize: '20px',
+          fontWeight: 'normal',
+          fontStyle: 'normal',
+          fontStretch: 'normal',
+          lineHeight: 'normal',
+          letterSpacing: 'normal',
+          color: '#f5c923'
+        }}>          
+          <span>{intl.get("BANNER_TITLE")}</span>
+        </Title>
+
         </Col>
       </Row>
     </Header>
